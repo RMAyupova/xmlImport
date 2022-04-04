b2ff6496145423ca54d6f98a932ec2aca4a187e9
ramila <ramilya@example.ru>
2022-04-04 15:29:58 +0300

    класс мастер

---
+++ Models/Master.cs
@@ -0,0 +1,24 @@
+﻿namespace XmlImport.Models
+{
+    public class Master
+    {
+        public int Id { get; set; }
+        public string CIK { get; set; }
+        public string CompanyName { get; set; }
+        public string FormType { get; set; }//через ковычки
+        public string DateFiled { get; set; }
+        public string FileName { get; set; }//через ковычки
+
+        public Master()  //пустой конструктор
+        {
+        }
+        public Master(string cik, string companyName, string formType, string dateFiled, string fileName)
+        {
+            CIK = cik;
+            CompanyName = companyName;
+            FormType = formType;
+            DateFiled = dateFiled;
+            FileName = fileName;
+        }
+    }
+}
